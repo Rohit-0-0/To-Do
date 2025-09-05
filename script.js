d60a979cada9e5ea4780333ffda739f780818b0f
Rohit-0-0 <rohitpatel90878@gmail.com>
2025-09-05 16:55:31 +0530

    final

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 let addtaskbtn = document.getElementById("addtaskbtn");
 function addfunction() {
   let newtask = document.getElementsByTagName("ul");
-//   console.log(newtask)
   let newli = document.createElement("li");
   newli.setAttribute("class", "newli")
-//   console.log(newli);
   button = document.createElement("button");
   button.innerText = "x";
-//   console.log(button);
-//   console.log(button.innerText);
   let newtext = document.getElementById("yyy").value;
   newli.append(newtext);
   newli.append(button);
-//   console.log(newli.innerHTML);
   newtask[0].append(newli);
-
+  document.getElementById("yyy").value = "";
   //remove krne ka code
   button.addEventListener("click", () => {
     newli.remove();
